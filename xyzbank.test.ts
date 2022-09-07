769725087183a9e427cada1532837d9b7c2d43e5
KendraJones <kendralee.jones81@gmail.com>
2022-09-06 20:11:21 -0600

    updating

--- xyzbank.test.ts
+++ xyzbank.test.ts
@@ -2,8 +2,18 @@ import { xyzbank } from "./xyzbank";
 const fs = require('fs');
 const xyzBank = new xyzbank();
 
-test('', async () => {
-
+test('Customer Login', async () => {
+    await xyzBank.navigate()
+    await xyzBank.click(xyzBank.customerLogin)
+    await xyzBank.click(xyzBank.yourName)
+    await xyzBank.click(xyzBank.user)
+    await xyzBank.click(xyzBank.login)
+    let text = await xyzBank.getResults()
+    expect(text).toContain('Hermoine Granger')
+    await fs.writeFile(`${__dirname}/screenshots/custLogin.png`, await xyzBank.driver.takeScreenshot(), 'base64', (e) => {
+        if (e) console.error(e)
+        else console.log("Screenshot Successful!")
+    })
 });
 
 afterAll(async () => {
--- xyzbank.ts
+++ xyzbank.ts
@@ -2,10 +2,16 @@ import { By } from "selenium-webdriver";
 import { BasePage } from "./basePage";
 
 export class xyzbank extends BasePage {
-    something: By = By.id('')
+    customerLogin: By = By.className('btn btn-primary btn-lg')
+    yourName: By = By.id('userSelect')
+    user: By = By.xpath('//*[@id="userSelect"]/option[2]')
+    login: By = By.className('btn btn-default')
+    customerName: By = By.className('fontBig ng-binding')
 
     constructor() {
         super({url: "https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login"})
     };
-    
+    async getResults() {
+        return this.getText(this.customerName)
+    }
 };
