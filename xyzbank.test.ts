848abe9ea0a11538d296ed7c6b980579a737a470
KendraJones <kendralee.jones81@gmail.com>
2022-09-07 20:13:37 -0600

    updating tests

--- xyzbank.test.ts
+++ xyzbank.test.ts
@@ -6,7 +6,7 @@ test('Customer Login', async () => {
     await xyzBank.navigate()
     await xyzBank.click(xyzBank.customerLogin)
     await xyzBank.click(xyzBank.yourName)
-    await xyzBank.click(xyzBank.user)
+    await xyzBank.click(xyzBank.Hermione)
     await xyzBank.click(xyzBank.login)
     let text = await xyzBank.getResults()
     expect(text).toContain('Hermoine Granger')
@@ -14,6 +14,56 @@ test('Customer Login', async () => {
         if (e) console.error(e)
         else console.log("Screenshot Successful!")
     })
+});
+test('Testing both the ability to make a Deposit and then verifying the Transaction', async () => {
+    await xyzBank.navigate()
+    await xyzBank.click(xyzBank.customerLogin)
+    await xyzBank.click(xyzBank.yourName)
+    await xyzBank.click(xyzBank.Harry)
+    await xyzBank.click(xyzBank.login)
+    await xyzBank.click(xyzBank.deposit)
+    await xyzBank.Amount(500)
+    await xyzBank.click(xyzBank.addDeposit)
+    await fs.writeFile(`${__dirname}/screenshots/deposit.png`, await xyzBank.driver.takeScreenshot(), 'base64', (e) => {
+        if (e) console.error(e)
+        else console.log("Screenshot Successful!")
+    });
+    await xyzBank.driver.sleep(3000)
+    await xyzBank.click(xyzBank.transaction)
+    let trnsctn = await xyzBank.getDate()
+    expect (trnsctn).toContain('500')
+    await fs.writeFile(`${__dirname}/screenshots/depositTransact.png`, await xyzBank.driver.takeScreenshot(), 'base64', (e) => {
+        if (e) console.error(e)
+        else console.log("Screenshot Successful!")
+    });
+});
+test('Testing both the ability to make a Withdrawal and then verifying the Transaction', async () => {
+    await xyzBank.navigate()
+    await xyzBank.click(xyzBank.customerLogin)
+    await xyzBank.click(xyzBank.yourName)
+    await xyzBank.click(xyzBank.Albus)
+    await xyzBank.click(xyzBank.login)
+    await xyzBank.click(xyzBank.deposit)
+    await xyzBank.Amount(100)
+    await xyzBank.click(xyzBank.addDeposit)
+    await xyzBank.driver.sleep(2000)
+    await xyzBank.click(xyzBank.withdrawal)
+    await xyzBank.click(xyzBank.wdAmount)
+    await xyzBank.WDAmount(25)
+    await xyzBank.click(xyzBank.withdraw)
+    await xyzBank.driver.sleep(2000)
+    await fs.writeFile(`${__dirname}/screenshots/withdrawal.png`, await xyzBank.driver.takeScreenshot(), 'base64', (e) => {
+        if (e) console.error(e)
+        else console.log("Screenshot Successful!")
+    });
+    await xyzBank.driver.sleep(3000)
+    await xyzBank.click(xyzBank.transaction)
+    let transctn = await xyzBank.getAmt()
+    expect(transctn).toContain('25')
+    await fs.writeFile(`${__dirname}/screenshots/withdrawalTransact.png`, await xyzBank.driver.takeScreenshot(), 'base64', (e) => {
+        if (e) console.error(e)
+        else console.log("Screenshot Successful!")
+    });
 });
 
 afterAll(async () => {
--- xyzbank.ts
+++ xyzbank.ts
@@ -4,14 +4,39 @@ import { BasePage } from "./basePage";
 export class xyzbank extends BasePage {
     customerLogin: By = By.className('btn btn-primary btn-lg')
     yourName: By = By.id('userSelect')
-    user: By = By.xpath('//*[@id="userSelect"]/option[2]')
+    Hermione: By = By.xpath('//*[@id="userSelect"]/option[2]')
+    Harry: By = By.xpath('//*[@id="userSelect"]/option[3]')
     login: By = By.className('btn btn-default')
     customerName: By = By.className('fontBig ng-binding')
+
+    deposit: By = By.xpath('/html/body/div/div/div[2]/div/div[3]/button[2]')
+    amount: By = By.xpath('/html/body/div/div/div[2]/div/div[4]/div/form/div/input')
+    addDeposit: By = By.className('btn btn-default')
+    transaction: By = By.xpath('/html/body/div/div/div[2]/div/div[3]/button[1]')
+    dateTime: By = By.xpath('//*[@id="anchor0"]')
+
+    Albus: By = By.xpath('//*[@id="userSelect"]/option[5]')
+    withdrawal: By = By.xpath('/html/body/div/div/div[2]/div/div[3]/button[3]')
+    wdAmount: By = By.xpath('/html/body/div/div/div[2]/div/div[4]/div/form/div/input')
+    withdraw: By = By.xpath('/html/body/div/div/div[2]/div/div[4]/div/form/button')
+    getAmount: By = By.xpath('//*[@id="anchor1"]/td[2]')
 
     constructor() {
         super({url: "https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login"})
     };
     async getResults() {
         return this.getText(this.customerName)
+    };
+    async Amount(number: number) {
+        return this.setInput(this.amount, `${number}`)
+    };
+    async getDate() {
+        return this.getText(this.dateTime)
+    };
+    async WDAmount(wdamt: number) {
+        return this.setInput(this.wdAmount, `${wdamt}`)
+    };
+    async getAmt() {
+        return this.getText(this.getAmount)
     }
 };
