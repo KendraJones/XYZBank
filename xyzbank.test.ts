fefb19870f630ea70727068315c8117e6d3e6507
KendraJones <kendralee.jones81@gmail.com>
2022-09-07 20:19:23 -0600

    updating tests

--- xyzbank.test.ts
+++ xyzbank.test.ts
@@ -28,7 +28,7 @@ test('Testing both the ability to make a Deposit and then verifying the Transact
         if (e) console.error(e)
         else console.log("Screenshot Successful!")
     });
-    await xyzBank.driver.sleep(3000)
+    await xyzBank.driver.sleep(2500)
     await xyzBank.click(xyzBank.transaction)
     let trnsctn = await xyzBank.getDate()
     expect (trnsctn).toContain('500')
@@ -51,15 +51,15 @@ test('Testing both the ability to make a Withdrawal and then verifying the Trans
     await xyzBank.click(xyzBank.wdAmount)
     await xyzBank.WDAmount(25)
     await xyzBank.click(xyzBank.withdraw)
-    await xyzBank.driver.sleep(2000)
+    await xyzBank.driver.sleep(1500)
     await fs.writeFile(`${__dirname}/screenshots/withdrawal.png`, await xyzBank.driver.takeScreenshot(), 'base64', (e) => {
         if (e) console.error(e)
         else console.log("Screenshot Successful!")
     });
-    await xyzBank.driver.sleep(3000)
+    await xyzBank.driver.sleep(2700)
     await xyzBank.click(xyzBank.transaction)
     let transctn = await xyzBank.getAmt()
-    expect(transctn).toContain('25')
+    expect(transctn).toEqual('25')
     await fs.writeFile(`${__dirname}/screenshots/withdrawalTransact.png`, await xyzBank.driver.takeScreenshot(), 'base64', (e) => {
         if (e) console.error(e)
         else console.log("Screenshot Successful!")
